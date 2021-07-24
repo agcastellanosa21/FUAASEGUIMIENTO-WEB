0fd3a7df975cf9a9945c2aae69e5cb6d4d40bfcf
andres.castellanos <andres.castellanos@keyrus.com>
2021-07-24 13:49:20 -0500

    MongoJSONEncoder class is created, for the serialized results for Mongo queries

---
+++ app/config/MongoJSONEncoder.py
@@ -0,0 +1,14 @@
+from datetime import datetime, date
+from bson import ObjectId
+import isodate as iso
+from flask.json import JSONEncoder
+
+
+class MongoJSONEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, (datetime, date)):
+            return iso.datetime_isoformat(o)
+        if isinstance(o, ObjectId):
+            return str(o)
+        else:
+            return super().default(o)
