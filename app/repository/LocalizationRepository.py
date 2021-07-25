9793cfd64a5714b640320cdc8333ecc9abab562b
andres.castellanos <andres.castellanos@keyrus.com>
2021-07-24 21:47:21 -0500

    Create method is added to save the location in the Mongo DB database

--- app/repository/LocalizationRepository.py
+++ app/repository/LocalizationRepository.py
@@ -1,4 +1,5 @@
 from app.config import ConnectionDB
+from datetime import datetime
 
 
 class LocalizationRepository:
@@ -33,3 +34,14 @@ class LocalizationRepository:
     def findLocalizationHistoryByCart(self, plaque):
         localization = self.connection.db.localization.find({"vehicle": plaque}).sort("date", -1)
         return localization
+
+    # This method is used to record the locations of each of the carts
+    def create(self, body):
+        print(body)
+        self.connection.db.localization.save({
+            "vehicle": body["vehicle"],
+            "date": datetime.now(),
+            "longitude": body["longitude"],
+            "latitude": body["latitude"],
+            "gps_trace": body["gps_trace"]
+        })
